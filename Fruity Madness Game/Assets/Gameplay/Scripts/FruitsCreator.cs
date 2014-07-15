54dbbb1c5a743c3392ce09e6b01c6904cbf505d6
Eldar <timekillerg@gmail.com>
2014-07-16 00:50:21 +0300

    Eldar: Added project "Fruit Madness"

---
+++ Fruity Madness Game/Assets/Gameplay/Scripts/FruitsCreator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FruitsCreator : MonoBehaviour {
+
+    public GameObject[] Fruits;
+    public Vector3[] FruitsPositions;
+
+
+	void Start () {
+	    foreach(Vector3 fruitPosition in FruitsPositions)
+        {
+            int fruitId = UnityEngine.Random.Range(0, Fruits.Length);
+            Instantiate(Fruits[fruitId], fruitPosition, Quaternion.identity);
+        }
+	}
+	
+	
+	void Update () {
+	
+	}
+}
---
+++ Fruity Madness Game/Assets/Gameplay/Scripts/PlayerFruitMover.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFruitMover : MonoBehaviour
+{
+    public GameObject PlayerFruit;
+    public float PlayerSpeed;
+
+    public Material[] FruitsMaterials;
+
+    private Vector3 MouseClickPosition;
+    private RaycastHit hit;
+
+    void Start()
+    {
+        ChangeFruitMaterial();
+        hit = new RaycastHit();
+    }
+
+    void Update()
+    {
+    }
+
+    void OnMouseDown()
+    {
+
+    }
+
+    void OnMouseUp()
+    {
+        SetMousePosition();
+        AddVelocityToPlayerFruit();
+        ChangeFruitMaterial();
+    }
+
+    private void SetMousePosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 10000.0f))
+        {
+            Vector3 newpos = new Vector3(hit.point.x, 0.5f, hit.point.z);
+            MouseClickPosition = newpos;
+        }
+    }
+
+    private void AddVelocityToPlayerFruit()
+    {
+        PlayerFruit.rigidbody.velocity = (MouseClickPosition - PlayerFruit.rigidbody.transform.position) * PlayerSpeed;
+    }
+
+    private void ChangeFruitMaterial()
+    {
+        if (FruitsMaterials != null && PlayerFruit != null)
+        {
+            int materialId = UnityEngine.Random.Range(0, FruitsMaterials.Length);
+            PlayerFruit.gameObject.renderer.material = FruitsMaterials[materialId];
+        }
+    }
+}
